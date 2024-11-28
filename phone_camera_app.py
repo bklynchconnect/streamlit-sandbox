46eb53d65a8da91bbb51e0468e87db882b9b8f60
Brian-Vineland <brian.lynch@vinelandresearch.com>
2024-11-28 13:24:25 -0500

    updating if else to show none image

--- phone_camera_app.py
+++ phone_camera_app.py
@@ -4,7 +4,9 @@ from streamlit_back_camera_input import back_camera_input
 st.write("Camera test app")
 from camera_input_live import camera_input_live
 
-image = camera_input_live(debounce=100)
+image = camera_input_live(debounce=10)
 
 if image is not None:
     st.image(image)
+else:
+	st.write("none image")
