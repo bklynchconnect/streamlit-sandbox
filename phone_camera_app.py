47ee4f5c9db4fb1c843e8c1a7b7237b7b867627a
Brian-Vineland <brian.lynch@vinelandresearch.com>
2024-11-28 11:56:18 -0500

    added camera live src code

--- phone_camera_app.py
+++ phone_camera_app.py
@@ -2,5 +2,12 @@ import streamlit as st
 from streamlit_back_camera_input import back_camera_input
 
 st.write("Camera test app")
-pic = back_camera_input()
-st.image(pic)
+from camera_input_live import camera_input_live
+
+"# Streamlit camera input live Demo"
+"## Try holding a qr code in front of your webcam"
+
+image = camera_input_live()
+
+if image is not None:
+    st.image(image)
