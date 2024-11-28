28e6570f5b327062a07a605e594359c43304a533
Brian-Vineland <brian.lynch@vinelandresearch.com>
2024-11-28 11:59:38 -0500

    changed update freq

--- phone_camera_app.py
+++ phone_camera_app.py
@@ -4,10 +4,7 @@ from streamlit_back_camera_input import back_camera_input
 st.write("Camera test app")
 from camera_input_live import camera_input_live
 
-"# Streamlit camera input live Demo"
-"## Try holding a qr code in front of your webcam"
-
-image = camera_input_live()
+image = camera_input_live(debounce=100)
 
 if image is not None:
     st.image(image)
